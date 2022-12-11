3666c14cfd6f905626d656a066189031ae09943f
IlmariHirvonen <ilmarinukkeli@gmail.com>
2022-12-11 10:39:38 +0200

    made another version that is not viable since it is too tall

--- software/Quantizer test2/Quantizer test2.ino
+++ software/Quantizer test2/Quantizer test2.ino
@@ -1,6 +1,7 @@
 #include <Wire.h>//Include the Wire library to talk I2C
 #include <Arduino.h>
 #include <AceSorting.h>
+
 #define MCP4725_ADDR 0x60
 using ace_sorting::shellSortKnuth;
 
@@ -76,17 +77,33 @@ int all_notes[] = {0, 580, 580*2, 580*3, 580*4, 580*5, 580*6,
 
 //If making this into a better product there should be a way to select the notes and maybe using them to calculate
 //the minimum distance between notes to reduce unesecary time looking up and sending data.
-
+void printArray(int arr[], int arrLenght)
+{
+  for(int i = 0; i < arrLenght; i++)
+  {
+    Serial.println(arr[i]);
+  }
+}
 
 int number_of_notes = 0;
+
+//Since there are 12 buttons, each element will correspond to a button.
+int keyMask1[] = [1,1,1,1,1,1,1,1,1,1,1,1];
+int keyMask2[] = [1,1,1,1,1,1,1,1,1,1,1,1];
+
 void setup() {
   number_of_notes = sizeof(all_notes)/sizeof(all_notes[0]);
   shellSortKnuth(all_notes, number_of_notes);
+  
+
   Wire.begin();
   pinMode(A6, INPUT);
   Serial.begin(9600);
+  
+
+  printArray(all_notes, number_of_notes);
 } 
-int input1Pin = A6;
+int inputPin1 = A6;
 float CV1_in = 0;
 int CV1_pitch = 0;
 int CV1_quantized = 0;
@@ -101,7 +118,7 @@ void loop() {
   
   //Look up and send a new note if there is a possibility that a note could change.
   if(abs(CV1_pitch - CV1_quantized) > (48/2)){
-    Serial.println(CV1_quantized);
+    //Serial.println(CV1_quantized);
     CV1_quantized = search(all_notes, number_of_notes, CV1_pitch);
     Wire.beginTransmission(MCP4725_ADDR);
     Wire.write(64); 
@@ -111,3 +128,12 @@ void loop() {
   }
   delay(1);
 }
+
+
+/*
+  unsigned long startTime = micros();
+  unsigned long endTime = micros();
+  Serial.println("time to run");
+  Serial.println(endTime-startTime);
+  Serial.println("-------");
+*/
